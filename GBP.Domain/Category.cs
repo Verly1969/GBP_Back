2c950a509c07f4999ec98fc96ae73b1a2b5e1181
Verly1969 <eddyverly@icloud.com>
2026-05-21 16:46:00 +0200

    Ajout des entités Category, AccountType et CreditType
    Suppression de la classe vide Class1. Ajout du dossier "Enums" dans le projet. Création des entités Category (avec auto-référence), AccountType et CreditType dans le domaine, incluant leurs propriétés principales et des commentaires pour les futures propriétés de navigation.

---
+++ GBP.Domain/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GBP.Domain;
+
+public class Category
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public string? SubCategory { get; set; }
+    
+    // Auto référence
+    public int? ParentId { get; set; }
+
+    // Navigation properties
+    public Category? Parent { get; set; }
+    public ICollection<Category> SubCategories { get; set; } = new List<Category>();
+}
--- GBP.Domain/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace GBP.Domain
-{
-    public class Class1
-    {
-
-    }
-}
