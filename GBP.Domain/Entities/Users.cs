49ca5a78b144555e362931a16d77e55d01a9c4bc
Verly1969 <eddyverly@icloud.com>
2026-05-24 15:18:59 +0200

    Ajout enum Role et refonte de la classe Users
    Ajout de l’énumération Role (User, Admin) dans GBP.Domain.Enums. Réécriture complète de la classe Users avec de nouvelles propriétés requises (Id, LastName, FirstName, Email, SecretKeyHash, PasswordHash, Role, CreatedAt). Nettoyage du fichier csproj en supprimant la déclaration du dossier Enums.

--- GBP.Domain/Entities/Users.cs
+++ GBP.Domain/Entities/Users.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GBP.Domain.Enums;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,13 @@ namespace GBP.Domain.Entities;
 
 public class Users
 {
+    public required Guid Id { get; set; }
+    public required string LastName { get; set; }
+    public required string FirstName { get; set; }
+    public required string Email { get; set; }
+    public required string SecretKeyHash { get; set; }
+    public required string PasswordHash { get; set; }
+    public required Role Role { get; set; }
+    public required DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
 }
