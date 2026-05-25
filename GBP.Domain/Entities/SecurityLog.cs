2dc481507a3fcf9616e85a6610484324547b6e11
Verly1969 <eddyverly@icloud.com>
2026-05-25 14:51:58 +0200

    Ajoute entités Log, SecurityLog et enum Level
    Refonte de Log.cs pour inclure toutes les propriétés nécessaires à la gestion des logs, ajout de SecurityLog.cs pour tracer les accès et bannissements, et création de l'enum Level pour les niveaux de log.

---
+++ GBP.Domain/Entities/SecurityLog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GBP.Domain.Entities;
+
+public class SecurityLog
+{
+    public int Id { get; set; }
+    public required string IpAddress { get; set; }
+    public required string EndPoint { get; set; }
+    public DateTime DateAttempt { get; set; }
+    public string? UserAgent { get; set; }
+    public bool IsBanned { get; set; }
+    public DateTime? StartBan { get; set; }
+    public DateTime? EndBan { get; set; }
+    public string? BanRaison { get; set; }
+    public required string CreatedBy { get; set; }
+}
