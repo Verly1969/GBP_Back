20f8a3d3ad12ce73a0071162720d7ace27ab500e
Verly1969 <eddyverly@icloud.com>
2026-05-25 14:03:19 +0200

    Implémente les entités Transaction et TransactionType
    Ajoute la classe Transaction avec toutes ses propriétés, y compris les clés étrangères et les propriétés de navigation vers Account, Category et TransactionType. Crée également la classe TransactionType avec ses attributs et la collection de transactions associées pour structurer la gestion des transactions dans le domaine.

--- GBP.Domain/Entities/Transaction.cs
+++ GBP.Domain/Entities/Transaction.cs
@@ -2,9 +2,24 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace GBP.Domain.Entities
+namespace GBP.Domain.Entities;
+
+public class Transaction
 {
-    public class Transaction
-    {
-    }
+    public Guid Id { get; set; }
+    public decimal Amount { get; set; }
+    public DateTime DateOfTransaction { get; set; }
+    public string? Description { get; set; }
+
+    // Foreigen Keys
+    public Guid SourceAccountId { get; set; }
+    public Guid? TargetAccountId { get; set; }
+    public int CategoryId { get; set; }
+    public int TransactionTypeId { get; set; }
+
+    // Navigation Properties - Parent entities
+    public required Account SourceAccount { get; set; }
+    public required Account? TargetAccount { get; set; }
+    public required Category Categorie { get; set; }
+    public required TransactionType TransactionType { get; set; }
 }
---
+++ GBP.Domain/Entities/TransactionType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GBP.Domain.Entities;
+
+public class TransactionType
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public string? Description { get; set; }
+
+    // Navigation Properties - Child entitie
+    public ICollection<Transaction> Transactions { get; set; } = [];
+}
