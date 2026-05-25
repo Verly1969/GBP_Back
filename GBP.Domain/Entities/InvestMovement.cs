82978ed7d57384062bf867bb77a68f27fcd3bba2
Verly1969 <eddyverly@icloud.com>
2026-05-25 10:38:19 +0200

    Ajout des entités InvestMovement et InvestMovementType
    Définition des classes de domaine InvestMovement et InvestMovementType avec leurs propriétés, clés étrangères et relations de navigation pour modéliser les mouvements d’investissement et leurs types.

--- GBP.Domain/Entities/InvestMovement.cs
+++ GBP.Domain/Entities/InvestMovement.cs
@@ -2,9 +2,20 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace GBP.Domain.Entities
+namespace GBP.Domain.Entities;
+
+public class InvestMovement
 {
-    public class InvestMovement
-    {
-    }
+    public Guid Id { get; set; }
+    public decimal Quantity { get; set; }
+    public decimal UnitPrice { get; set; }
+    public decimal Charge { get; set; }
+
+    // Foreign keys
+    public Guid WalletId { get; set; }
+    public int InvestTypeId { get; set; }
+
+    // Navigation properties - Parent entities
+    public required Wallet Wallet { get; set; }
+    public required InvestMovementType InvestMovementType { get; set; }
 }
