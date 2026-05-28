ba5f90b74213244e6d0305a8ec4ee72e4a54228e
Verly1969 <eddyverly@icloud.com>
2026-05-28 16:51:01 +0200

    Ajout IPAsswordService et PasswordService

---
+++ GBP.Security/Services/Auth/PasswordService.cs
@@ -0,0 +1,29 @@
+﻿using GBP.Core.Interfaces.Services.Auth;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GBP.Security.Services.Auth
+{
+    public class PasswordService : IPasswordService
+    {
+        public string HashPassword(string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool VerifyPassword(string password, string hashedPassword)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    // Paramètres de configuration Argon2id
+    public record Argon2Params(
+        int HashLength,   // longueur du hash en octets
+        int SaltLength,   // longueur du sel en octets
+        int MemoryCost, // en Ko
+        int TimeCost,   // nombre d'itérations
+        int Parallelism // nombre de threads
+    );
+}
