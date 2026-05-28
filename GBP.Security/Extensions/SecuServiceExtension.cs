be83fdb2e71cc834029d98eb69fe0be5b1db39db
Verly1969 <eddyverly@icloud.com>
2026-05-28 12:01:07 +0200

    Ajouter le Middleware de sécurité

--- GBP.Security/Extensions/SecuServiceExtension.cs
+++ GBP.Security/Extensions/SecuServiceExtension.cs
@@ -1,5 +1,9 @@
-﻿using GBP.Core.Interfaces.Services;
+﻿using GBP.Core.Interfaces.Repositories;
+using GBP.Core.Interfaces.Services;
+using GBP.Infra.Repositories;
+using GBP.Security.Middlewares;
 using GBP.Security.Services.Auth;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -13,6 +17,19 @@ namespace GBP.Security.Extensions
         public static void AddSecuService(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddScoped<IAuthService, AuthService>();
+            services.AddScoped<ISecurityLogRepository, SecurityLogRepository>();
+            services.AddMemoryCache();
+        }
+
+        // Activer le middleware de sécurité dans le pipeline de requêtes
+        public static void UseSecuMiddleware(this IApplicationBuilder application)
+        {
+            // Note: Le middleware lui-même n'est pas enregistré en tant que service
+            // car il est ajouté directement dans le pipeline de requêtes via
+            // app.UseMiddleware<SecurityMiddleware>()
+            // Cependant, les services dont le middleware dépend (comme ISecurityLogRepository)
+            // doivent être enregistrés
+            application.UseMiddleware<SecurityMiddleware>();
         }
     }
 }
