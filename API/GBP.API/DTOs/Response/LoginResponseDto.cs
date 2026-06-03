75b03666a0ce1330be862c1c91472cbc30b0b188
Verly1969 <eddyverly@icloud.com>
2026-06-03 17:46:11 +0200

    Suppression de l’authentification et gestion utilisateurs
    Suppression complète de l’API d’authentification, des entités, repositories, services, DTOs, configurations EF Core et fichiers liés à la gestion des utilisateurs, de la sécurité et des logs. Nettoyage des références et des fichiers de configuration associés.

--- API/GBP.API/DTOs/Response/LoginResponseDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GBP.API.DTOs.Response
-{
-    public class LoginResponseDto
-    {
-        public Guid Id { get; set; }
-        public required string FirstName { get; set; }
-        public required string LastName { get; set; }
-    }
-}
